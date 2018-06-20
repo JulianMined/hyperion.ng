8306573db2d9b9f4590353e1e7bbea67eac9963b
Julian <dj-jb@hotmail.de>
2018-06-20 17:25:39 +0200

    Compile errors fixed

--- cmake/Toolchain-rpi.cmake
+++ cmake/Toolchain-rpi.cmake
@@ -1,4 +1,5 @@
 SET(RASPCROSS_DIR $ENV{HOME}/raspberrypi)
+SET(PIROOT ${RASPCROSS_DIR}/rootfs)
 
 SET(CMAKE_SYSTEM_NAME Linux) 
 SET(CMAKE_SYSTEM_VERSION 1) 
@@ -7,8 +8,20 @@ SET(CMAKE_SYSTEM_VERSION 1)
 SET(CMAKE_C_COMPILER   ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER ${RASPCROSS_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-g++)
 
+
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH ${RASPCROSS_DIR}/rootfs) 
+SET(FLAGS "-Wl,-rpath-link,${PIROOT}/opt/vc/lib -Wl,-rpath-link,${PIROOT}/lib/arm-linux-gnueabihf -Wl,-rpath-link,${PIROOT}/usr/lib/arm-linux-gnueabihf -Wl,-rpath-link,${PIROOT}/usr/local/lib")
+
+UNSET(CMAKE_C_FLAGS CACHE)
+UNSET(CMAKE_CXX_FLAGS CACHE)
+
+SET(CMAKE_CXX_FLAGS ${FLAGS} CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS ${FLAGS} CACHE STRING "" FORCE)
+
+SET(CMAKE_SYSROOT ${PIROOT})
+SET(CMAKE_FIND_ROOT_PATH ${PIROOT})
+ 
+#INCLUDE_DIRECTORIES(${RASPCROSS_DIR}/rootfs/opt/vs/lib)
 
 # search for programs in the build host directories 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
